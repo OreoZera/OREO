5adb5c5bb84cc87aa6012422e759318e28f72ad1
Le Thanh Thien <89253129+CallmeSun3K@users.noreply.github.com>
2021-08-21 04:41:42 +0000

    Add files via upload

--- modules/commands/uptime.js
+++ modules/commands/uptime.js
@@ -1,40 +1,35 @@
-﻿module.exports.config = {
-	name: "uptime",
-	version: "1.0.2",
-	hasPermssion: 0,
-	credits: "Mirai Team",
-	description: "Kiểm tra thời gian bot đã online",
-	commandCategory: "system",
-	cooldowns: 5,
-	dependencies: {
-		"pidusage": ""
-	}
+module.exports.config = {
+ name: "uptime",
+ version: "1.0.1",
+ hasPermssion: 0,
+ credits: "Mirai Team",
+ description: "Kiểm tra thời gian bot đã online",
+ commandCategory: "system",
+ cooldowns: 5,
+ dependencies: {
+  "pidusage": ""
+ }
 };
 
 function byte2mb(bytes) {
-	const units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-	let l = 0, n = parseInt(bytes, 10) || 0;
-	while (n >= 1024 && ++l) n = n / 1024;
-	return `${n.toFixed(n < 10 && l > 0 ? 1 : 0)} ${units[l]}`;
+ const units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+ let l = 0, n = parseInt(bytes, 10) || 0;
+ while (n >= 1024 && ++l) n = n / 1024;
+ return `${n.toFixed(n < 10> 0 ? 1 : 0)} ${units[l]}`;
 }
 
-module.exports.languages = {
-	"vi": {
-		"returnResult": "Bot đã hoạt động được %1 giờ %2 phút %3 giây.\n\n❯ Tổng người dùng: %4\n❯ Tổng Nhóm: %5\n❯ Cpu đang sử dụng: %6%\n❯ Ram đang sử dụng: %7\n❯ Ping: %8ms\n\n== This bot of CallmeSun =="
-	},
-	"en": {
-		"returnResult": "Bot has been working for %1 hour(s) %2 minute(s) %3 second(s).\n\n❯ Total users: %4\n❯ Total Threads: %5\n❯ Cpu usage: %6%\n❯ RAM usage: %7\n❯ Ping: %8ms\n\n=== This bot was made by CatalizCS and SpermLord ==="
-	}
-}
+module.exports.run = async ({ api, event, args }) => {
+ const time = process.uptime(),
+  hours = Math.floor(time / (60 * 60)),
+  minutes = Math.floor((time % (60 * 60)) / 60),
+  seconds = Math.floor(time % 60);
+ const axios = global.nodemodule["axios"];
+ const pidusage = await global.nodemodule["pidusage"](process.pid);
+ const res = await axios.get(`https://api.vangbanlanhat.tk/other?type=calendar`);
+ var hour = res.data.data.time.hour;
+ var minute = res.data.data.time.minute;
+ var second = res.data.data.time.second;
 
-module.exports.run = async ({ api, event, getText }) => {
-	const time = process.uptime(),
-		hours = Math.floor(time / (60 * 60)),
-		minutes = Math.floor((time % (60 * 60)) / 60),
-		seconds = Math.floor(time % 60);
-
-	const pidusage = await global.nodemodule["pidusage"](process.pid);
-
-	const timeStart = Date.now();
-	return api.sendMessage("", event.threadID, () => api.sendMessage(getText("returnResult", hours, minutes, seconds, global.data.allUserID.length, global.data.allThreadID.length, pidusage.cpu.toFixed(1), byte2mb(pidusage.memory), Date.now() - timeStart), event.threadID, event.messageID));
+ const timeStart = Date.now();
+ return api.sendMessage("", event.threadID, () => api.sendMessage(`Bây giờ là: ${hour}:${minute}:${second} \n⚡️Bot đã hoạt động ${hours} giờ ${minutes} phút ${seconds} giây.\n\n👤Tổng người dùng: ${global.data.allUserID.length}\n👥Tổng Nhóm: ${global.data.allThreadID.length}\n❯ Cpu đang sử dụng: ${pidusage.cpu.toFixed(1)}\n❯ Ram đang sử dụng: ${byte2mb(pidusage.memory)}\n❯ Ping: ${Date.now() - timeStart}ms\n\n== This bot of CallmeSun ==`, event.threadID, event.messageID));
 }
